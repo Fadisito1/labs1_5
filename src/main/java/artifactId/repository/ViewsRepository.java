5f3de263db89e4238034dab34da6409be815b9ce
David Morales Velez <166939100+Fadisito1@users.noreply.github.com>
2024-10-16 18:37:51 -0500

    views and viewsRepository

---
+++ src/main/java/artifactId/repository/ViewsRepository.java
@@ -0,0 +1,9 @@
+package artifactId.repository;
+
+import artifactId.model.views;
+import java.time.LocalDateTime;
+
+public interface ViewsRepository {
+
+    void addView(String usuario, LocalDateTime viewTime, int edad);
+}
