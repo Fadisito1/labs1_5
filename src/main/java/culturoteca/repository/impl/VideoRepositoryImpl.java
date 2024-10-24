9d1f9816f08b01adebfbaa35d05ec74f510e2b90
David Morales Velez <166939100+Fadisito1@users.noreply.github.com>
2024-10-23 22:20:00 -0500

    resolve min error

--- src/main/java/culturoteca/repository/impl/VideoRepositoryImpl.java
+++ src/main/java/culturoteca/repository/impl/VideoRepositoryImpl.java
@@ -1,7 +1,7 @@
 package culturoteca.repository.impl;
+
 import java.util.ArrayList;
 import java.util.List;
-
 import culturoteca.exceptions.VideoNotFoundException;
 import culturoteca.model.Video;
 import culturoteca.repository.VideoRepository;
@@ -17,25 +17,22 @@ public class VideoRepositoryImpl implements VideoRepository {
     @Override
     public List<Video> findAll() {
         if (videos.isEmpty()) {
-            throw new VideoNotFoundException("No se encontraron videos.");
+            throw new VideoNotFoundException("No videos found.");
         }
-        return videos;
+        return new ArrayList<>(videos); // Devuelve una copia de la lista de videos
     }
 
     @Override
     public Video save(Video video) {
-        this.videos.add( video );
+        this.videos.add(video);
         return video;
     }
 
     @Override
     public List<Video> find(String title) {
-        List<Video> filteredVideos = null;
-        for ( Video video : videos ) {
-            if(video.titulo().contains( title )){
-                if(filteredVideos == null){
-                    filteredVideos = new ArrayList<>();
-                }
+        List<Video> filteredVideos = new ArrayList<>();
+        for (Video video : videos) {
+            if (video.titulo().contains(title)) {
                 filteredVideos.add(video);
             }
         }
@@ -50,13 +47,9 @@ public class VideoRepositoryImpl implements VideoRepository {
                 filteredVideos.add(video);
             }
         }
-
-        if (filteredVideos.isEmpty()) {
-            System.out.println("No se encontraron videos en el rango de duración solicitado.");
-        }
-
         return filteredVideos;
     }
+
     @Override
     public List<Video> findByTitle(String title) {
         List<Video> matchedVideos = new ArrayList<>();
@@ -65,10 +58,6 @@ public class VideoRepositoryImpl implements VideoRepository {
                 matchedVideos.add(video);
             }
         }
-        if (matchedVideos.isEmpty()) {
-            System.out.println("El video solicitado no se encuentra.");
-        }
         return matchedVideos;
     }
-
 }
